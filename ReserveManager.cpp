47ab13eeb249533e0b389faf80adb50baf3ce8d9
Roni <ronyca99@hotmail.com>
2021-04-21 19:53:47 -0600

    upddate

--- ReserveManager.cpp
+++ ReserveManager.cpp
@@ -4,19 +4,17 @@
 
 #include "ReserveManager.h"
 void ReserveManager::agregarPasajero(vector<Bus*> vectorBuses,Passanger* passanger) {
-   // for (int i = 0; i <= vectorBuses.size(); i++) {
-       // int Maxsize = (vectorBuses.data()[0].getSitCapacity()) / 2;
-    vectorBuses[0]->addPassanger(passanger);
+    for (int i = 0; i <= vectorBuses.size(); i++) {
+        int Maxsize = (vectorBuses.data()[0]->getSitCapacity()) / 2;
     cout<<vectorBuses[0]->getPassangerList().size()<<endl;// 1
-    //    if (vectorBuses.data()[0].getPassangerList().size() < Maxsize) {
-      //      vectorBuses.data()[0].addPassanger(passanger);
-           // cout<<vectorBuses.data()[i].getPassangerList().size()<<endl;
-            //vectorBuses.data()[i].getPassangerList().resize(6);
-           // cout<<vectorBuses.data()[i].getPassangerList().size()<<endl;
-          //  break;
-     //   }
-   // }
-    //cout<<vectorBuses.data()[0].getPassangerList().size()<<endl;
+        if (vectorBuses.data()[0]->getPassangerList().size() < Maxsize) {
+            vectorBuses.data()[0]->addPassanger(passanger);
+            vectorBuses.data()[0]->addPassanger(passanger);
+           // vectorBuses.data()[i]->getPassangerList().resize(vectorBuses.data()[0]->getPassangerList().size() + 1);
+            break;
+       }
+    }
+
 }
 void ReserveManager::agregarGrupoDePasajeros(vector<Bus> vectorBuses, Passanger* passanger1){
 
--- main.cpp
+++ main.cpp
@@ -14,14 +14,19 @@ int main() {
     Bus* bus2  = new Bus("dad","dad","dad","dad",20);
 
     Passanger* passanger1 = new Passanger("PEPE");
+    Passanger* passanger2 = new Passanger("Pablo");
 
     busManager.addBus(bus1);
     busManager.addBus(bus2);
 
     reserveManager.agregarPasajero(busManager.getBusList(),passanger1);
     cout<<"Main"<<endl;
-    cout<<busManager.getBusList().data()[0]->getPassangerList().size();
+    cout<<busManager.getBusList().data()[0]->getPassangerList().size()<<endl;
+    reserveManager.agregarPasajero(busManager.getBusList(),passanger2);
+    cout<<"Main"<<endl;
+    cout<<busManager.getBusList().data()[0]->getPassangerList().size()<<endl;
 
+    cout<<busManager.getBusList()[0]->getPassangerList()[2].toString();
 
 
 
